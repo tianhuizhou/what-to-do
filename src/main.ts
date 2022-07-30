9c791ce04237702297db2a3fbbdcaf060a8c1074
Bobbyzzz <tianhui@ndmglobal.com>
2022-07-30 03:28:56 -1000

    init: install dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.use(store)
+app.use(router)
+
+app.mount('#app')
