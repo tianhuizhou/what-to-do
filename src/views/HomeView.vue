20ecddbb9242259a314c1efa0179e9aeede434b5
Bobbyzzz <tianhui@ndmglobal.com>
2022-07-29 23:38:47 -1000

    init: init project settings

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,37 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    {{ user.name }}
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+  import { defineComponent, reactive } from 'vue'
+  import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
-export default defineComponent({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-});
+  export default defineComponent({
+    name: 'HomeView',
+    components: {
+      HelloWorld,
+    },
+    setup() {
+      interface User {
+        id: number
+        name: string
+        settings: (string | number)[]
+      }
+
+      const user: User = reactive({
+        id: 1,
+        name: 'Bob',
+        settings: [12, 'asd'],
+      })
+
+      return {
+        // Reactive data
+        user,
+        // Computed variable
+      }
+    },
+  })
 </script>
---
+++ vue.config.js
@@ -0,0 +1,86 @@
+module.exports = {
+  publicPath: process.env.BASE_URL || '/',
+  lintOnSave: false,
+
+  productionSourceMap: false,
+
+  // filename with hash breaks PWA
+  filenameHashing: false,
+
+  pwa: {
+    name: 'WhatToDo',
+    themeColor: '#3C75A7',
+    msTileColor: '#2C2C2C',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
+
+  css: {
+    extract: false,
+
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          quietDeps: true,
+        },
+      },
+    },
+  },
+
+  chainWebpack(config) {
+    config.when(process.env.NODE_ENV !== 'development', (config) => {
+      config.optimization.splitChunks({
+        // chunks: 'all',
+        cacheGroups: {
+          initial_libs: {
+            name: 'chunk_libs_initial',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: 'initial', // only package third parties that are initially dependent
+          },
+          libs: {
+            name: 'chunk_libs',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 5,
+          },
+          element_ui: {
+            name: 'chunk_element', // split elementUI into a single package
+            test: /[\\/]node_modules[\\/]element-ui(.*)/, // in order to adapt to cnpm
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            reuseExistingChunk: true,
+          },
+          styles: {
+            name: 'chunk_styles',
+            test: /[\\/]src[\\/]assets[\\/]scss(.*)/,
+            priority: 15,
+            reuseExistingChunk: true,
+          },
+          cytoscape: {
+            name: 'chunk_cytoscape',
+            test: /[\\/]node_modules[\\/]cytoscape(.*)/,
+            priority: 20,
+            reuseExistingChunk: true,
+          },
+          pdfjs: {
+            name: 'chunk_pdfjs',
+            test: /[\\/]node_modules[\\/]pdfjs(.*)/,
+            priority: 20,
+            reuseExistingChunk: true,
+          },
+          common: {
+            name: 'common',
+            test: /[\\/]src[\\/]utils[\\/](.*)/,
+            priority: 10,
+            reuseExistingChunk: true,
+          },
+          components: {
+            name: 'chunk_components',
+            test: /[\\/]src[\\/]components[\\/](.*)/,
+            priority: 5,
+            reuseExistingChunk: true,
+          },
+        },
+      })
+    })
+  },
+}
